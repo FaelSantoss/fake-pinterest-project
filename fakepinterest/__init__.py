1c02380208b147ab70864454df8a7a360d71eebf
FaelSantoss <silva.faelm@gmail.com>
2024-03-24 16:54:15 -0300

    criação da funcionalidade de postar fotos

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -6,6 +6,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///fakepinterest.db"
 app.config["SECRET_KEY"] = "4040"
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
